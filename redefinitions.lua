5e0201968e18d6fe614801e88a8ee960a976377a
Calinou <calinou@opmbx.org>
2015-01-23 19:07:41 +0100

    Make rail crafting give 24 rails, remove redundant glass and obsidian glass redefinitions.

--- redefinitions.lua
+++ redefinitions.lua
@@ -33,7 +33,7 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = "default:rail 16",
+	output = "default:rail 24",
 	recipe = {
 		{"default:steel_ingot", "", "default:steel_ingot"},
 		{"default:steel_ingot", "default:stick", "default:steel_ingot"},
@@ -46,16 +46,8 @@ minetest.register_craft({
 	additional_wear = -0.15, -- Tool repair buff (15% bonus instead of 2%).
 })
 
--- Redefinitions of some default nodes:
-
--- Make glass and obsidian glass framed, like the More Blocks glasses:
-minetest.override_item("default:glass", {
-	drawtype = "glasslike_framed",
-})
-
-minetest.override_item("default:obsidian_glass", {
-	drawtype = "glasslike_framed",
-})
+-- Redefinitions of some default nodes
+-- ===================================
 
 -- Let there be light. This makes some nodes let light pass through:
 minetest.override_item("default:ladder", {
