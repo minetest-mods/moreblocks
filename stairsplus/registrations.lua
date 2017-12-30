c6f26203678365c0d58fa03d5d3195c19e96e731
Foz <fozolo@gmail.com>
2017-12-30 10:30:40 -0500

    Finish aliasing split_stone_tile_alt
    to checker_stone_tile.

--- stairsplus/registrations.lua
+++ stairsplus/registrations.lua
@@ -107,3 +107,23 @@ if minetest.get_modpath("wool") then
 		stairsplus:register_all(mod, name, nodename, ndef)
 	end
 end
+
+-- Alias cuts of split_stone_tile_alt which was renamed checker_stone_tile.
+stairsplus:register_alias_all("moreblocks", "split_stone_tile_alt", "moreblocks", "checker_stone_tile")
+
+-- The following LBM is necessary because the name stair_split_stone_tile_alt
+-- conflicts with another node and so the alias for that specific node gets
+-- ignored.
+minetest.register_lbm({
+	name = "moreblocks:fix_split_stone_tile_alt_name_collision",
+	nodenames = {"moreblocks:stair_split_stone_tile_alt"},
+	action = function(pos, node)
+		minetest.set_node(pos, {
+			name = "moreblocks:stair_checker_stone_tile",
+			param2 = minetest.get_node(pos).param2
+
+		})
+		minetest.log('action', "LBM replaced " .. node.name ..
+			" at " .. minetest.pos_to_string(pos))
+	end,
+})
