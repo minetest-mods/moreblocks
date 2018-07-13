5e441b03fa7729a5f4d3127879e849b454bb5ff9
codexp <codexp@gmx.net>
2018-07-13 12:52:11 +0200

    implement original intllib helper

--- init.lua
+++ init.lua
@@ -9,29 +9,24 @@ Licensed under the zlib license. See LICENSE.md for more information.
 --]]
 
 moreblocks = {}
+moreblocks.modname = minetest.get_current_modname()
+moreblocks.modpath = minetest.get_modpath(moreblocks.modname)
 
-local S
-if minetest.global_exists("intllib") then
-	if intllib.make_gettext_pair then
-		S = intllib.make_gettext_pair()
-	else
-		S = intllib.Getter()
-	end
-else
-	S = function(s) return s end
-end
+-- Load support for intllib.
+local MP = moreblocks.modpath
+local S = dofile(MP .. "/intllib.lua")
+
 moreblocks.intllib = S
 
-local modpath = minetest.get_modpath("moreblocks")
-
-dofile(modpath .. "/config.lua")
-dofile(modpath .. "/circular_saw.lua")
-dofile(modpath .. "/stairsplus/init.lua")
-dofile(modpath .. "/nodes.lua")
-dofile(modpath .. "/redefinitions.lua")
-dofile(modpath .. "/crafting.lua")
-dofile(modpath .. "/aliases.lua")
+-- Load mod's modules
+dofile(MP .. "/config.lua")
+dofile(MP .. "/circular_saw.lua")
+dofile(MP .. "/stairsplus/init.lua")
+dofile(MP .. "/nodes.lua")
+dofile(MP .. "/redefinitions.lua")
+dofile(MP .. "/crafting.lua")
+dofile(MP .. "/aliases.lua")
 
 if minetest.settings:get_bool("log_mods") then
-	minetest.log("action", S("[moreblocks] loaded."))
+	minetest.log("action", S("[MOD] moreblocks loaded."))
 end
