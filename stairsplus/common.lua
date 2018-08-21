21a5e4a58fc98c203d00b76d75ff68ef36626284
sys4-fr <sys4@free.fr>
2018-08-21 19:10:41 +0200

    Fix node rotation when source node is defined with place_param2 property setted to 0. (#123)
    However, this fix disable the global world texture orientation for fixed nodes in minetest 0.5-dev

--- stairsplus/common.lua
+++ stairsplus/common.lua
@@ -30,6 +30,7 @@ stairsplus.register_single = function(category, alternate, info, modname, subnam
 	end
 	def.paramtype = "light"
 	def.paramtype2 = def.paramtype2 or "facedir"
+	def.place_param2 = nil
 	def.on_place = minetest.rotate_node
 	if category ~= "slab" then
 		def.description = S("%s " .. descriptions[category]):format(fields.description)
