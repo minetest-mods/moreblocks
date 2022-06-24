c57954390f4d1e6977b552e3e54a09ca8f85a0ee
flux <25628292+fluxionary@users.noreply.github.com>
2022-06-24 11:58:04 -0700

    awareness of mineclone groups

--- stairsplus/groups/other.lua
+++ stairsplus/groups/other.lua
@@ -1,5 +1,6 @@
 -- group overrides for various other mods
 
+-- various things found in the wild
 stairsplus.api.register_passthrough_groups({
 	"bendy",
 	"dig_generic",
@@ -14,4 +15,47 @@ stairsplus.api.register_passthrough_groups({
 stairsplus.api.register_ignore_groups({
 	"type_node",
 	"ud_param2_colorable",
+})
+
+-- mineclone
+stairsplus.api.register_passthrough_groups({
+	"pickaxey",
+	"axey",
+	"shovely",
+	"swordy",
+	"swordy_cobweb",
+	"shearsy",
+	"shearsy_wool",
+	"handy",
+	"creative_breakable",
+	"dig_by_water",
+	"destroy_by_lava_flow",
+	"dig_by_piston",
+	"non_mycelium_plant",
+	"enderman_takable",
+	"disable_suffocation",
+	"no_rename",
+})
+
+stairsplus.api.register_scaling_groups({
+	"falling_node_damage",
+	"fire_encouragement",
+	"fire_flammability",
+	"comparator_signal",
+	"set_on_fire",
+	"compostability",
+})
+
+stairsplus.api.register_ignore_groups({
+	"crush_after_fall",
+	"cultivatable",
+	"path_creation_possible",
+	"spreading_dirt_type",
+	"dirtifies_below_solid",
+	"dirtifier",
+	"destroys_items",
+	"no_eat_delay",
+	"can_eat_when_full",
+	"attached_node_facedir",
+	"supported_node",
 })
