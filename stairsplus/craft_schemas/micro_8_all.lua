75969eb9d7a4169ca27383e816d4f7fd17e06feb
flux <25628292+fluxionary@users.noreply.github.com>
2022-06-26 20:37:20 -0700

    paranoid bugfixing

--- stairsplus/craft_schemas/micro_8_all.lua
+++ stairsplus/craft_schemas/micro_8_all.lua
@@ -2,7 +2,7 @@ local api = stairsplus.api
 local register_craft_schema = api.register_craft_schema
 
 for shape, shape_def in pairs(api.registered_shapes) do
-	if shape ~= "micro_8" then
+	if shape ~= "micro_8" and math.floor(shape_def.eighths) > 0 then
 		register_craft_schema({
 			type = "shapeless",
 			output = ("micro_8 %i"):format(shape_def.eighths),
